35a729a92bee786976bceda907e4836de5a067e1
Murat Celik <cashkl@yandex.com>
2018-05-05 21:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function breakOut(array,changeValue,stopValue) {
   array.foreach(element => {
-    if(stopValue==element) break;
+    if(stopValue===element) break;
     element=changeValue;
+  })
+}
+
+
+function keepGoing(array,changeValue,stopValue) {
+  array.foreach(element => {
+    if(stopValu===element) continue;
+    element=changeValue;
+  })
+}
+
+function findBy(array,findFn) {
+  array.foreach(element => {
+    if(element===findFn) return element
   })
 }
